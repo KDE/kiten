ec23d339a06f54fc381bb17a1328137f018c0c36
Neil Stevens <neil@qualityassistant.com>
2001-12-17 00:21:50 +0000

    compile
    svn path=/trunk/kdeedu/kiten/; revision=127491

--- kiten.cpp
+++ kiten.cpp
@@ -37,8 +37,8 @@ TopLevel::TopLevel(QWidget *parent, const char *name) : KMainWindow(parent, name
 {
 
 	Accel = new KGlobalAccel(this);
-	Accel->insertAction(i18n("Lookup Kanji (Kanjidic)"), i18n("Gives detailed information about Kanji currently on clipboard."), /* QString::null, */ KShortcuts("CTRL + ALT + K"), KShortcuts("CTRL + ALT + K"), this, SLOT(kanjiSearchAccel()));
-	Accel->insertAction(i18n("Lookup English/Japanese word"), i18n("Looks up current text on clipboard in the same way as if you used Kiten's regular search."), /* QString::null, */ KShortcuts("CTRL + ALT + S"), KShortcuts("CTRL + ALT + S"), this, SLOT(searchAccel()));
+	Accel->insertAction(i18n("Lookup Kanji (Kanjidic)"), i18n("Gives detailed information about Kanji currently on clipboard."), QString::null, KShortcuts("CTRL + ALT + K"), KShortcuts("CTRL + ALT + K"), this, SLOT(kanjiSearchAccel()));
+	Accel->insertAction(i18n("Lookup English/Japanese word"), i18n("Looks up current text on clipboard in the same way as if you used Kiten's regular search."), QString::null, KShortcuts("CTRL + ALT + S"), KShortcuts("CTRL + ALT + S"), this, SLOT(searchAccel()));
 	Accel->readSettings(KGlobal::config());
 	Accel->updateConnections();
 
