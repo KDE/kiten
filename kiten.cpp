9305adfbe41b5affcdfec26e5223d07eeaa9ae9e
Jason Katz-Brown <jason@katzbrown.com>
2002-02-01 22:13:54 +0000

    hopefully, this makes the KGlobalActions compile. I've just checked out the new kglobalaccel.h, and have not actually compiled it.. must update kdelibs, which I will do soon. but it should work now...
    svn path=/trunk/kdeedu/kiten/; revision=134968

--- kiten.cpp
+++ kiten.cpp
@@ -39,8 +39,8 @@ TopLevel::TopLevel(QWidget *parent, const char *name) : KMainWindow(parent, name
 {
 
 	Accel = new KGlobalAccel(this);
-	//Accel->insertAction(i18n("Lookup Kanji (Kanjidic)"), i18n("Gives detailed information about Kanji currently on clipboard."), QString::null, KKeySequence("CTRL + ALT + K"), KKeySequence("CTRL + ALT + K"), this, SLOT(kanjiSearchAccel()));
-	//Accel->insertAction(i18n("Lookup English/Japanese word"), i18n("Looks up current text on clipboard in the same way as if you used Kiten's regular search."), QString::null, KKeySequence("CTRL + ALT + S"), KKeySequence("CTRL + ALT + S"), this, SLOT(searchAccel()));
+	(void) Accel->insert("Lookup Kanji (Kanjidic)", i18n("Lookup Kanji (Kanjidic)"), i18n("Gives detailed information about Kanji currently on clipboard."), CTRL + ALT + Key_K, CTRL + ALT + Key_K, this, SLOT(kanjiSearchAccel()));
+	(void) Accel->insert("Lookup English/Japanese word", i18n("Lookup English/Japanese word"), i18n("Looks up current text on clipboard in the same way as if you used Kiten's regular search."), CTRL + ALT + Key_S, CTRL + ALT + Key_S, this, SLOT(searchAccel()));
 	Accel->readSettings(KGlobal::config());
 	Accel->updateConnections();
 
