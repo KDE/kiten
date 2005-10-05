820c838745d7338ff0ed4db3a12f166244aacf79
Dirk Mueller <mueller@kde.org>
2005-10-05 09:03:00 +0000

    SVN_SILENT: fix typo
    svn path=/trunk/KDE/kdeedu/kiten/; revision=467420

--- xjdxgen.c
+++ xjdxgen.c
@@ -17,7 +17,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston, MA  02110-1301, USA.     */
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.     */
 
 /* Changed: ignore all rc stuff. use args 1 and 2 for input/output file.
   -- jason */
